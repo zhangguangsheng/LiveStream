284f79d081da494f2693ac0a2aa9c31df32ed702
fanlb <callback.gz@gmail.com>
2017-04-25 20:30:24 +0800

    修改包名

---
+++ app/src/main/java/com/team/finn/MainActivity.java
@@ -0,0 +1,20 @@
+package com.team.finn;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+
+
+/**
+ *  版本号：1.0
+ *  备注消息：
+ **/
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
