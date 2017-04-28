f6776859e3f78ada4dbfde553a72e35883d74798
fanlb <callback.gz@gmail.com>
2017-04-28 22:44:47 +0800

    更改APP名称、icon，添加欢迎页

---
+++ app/src/main/java/com/team/finn/view/common/activity/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.team.finn.view.common.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.team.finn.R;
+
+public class SplashActivity extends AppCompatActivity {
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (isFinishing()) {
+                    return;
+                }
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
+    }
+}
